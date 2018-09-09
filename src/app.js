4650094916f5d6691042872cdc87d6178bc6412b
tzc <491514218@qq.com>
2018-09-09 21:21:41 +0800

    检测group子元素是否符合规则。

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 import Vue from 'vue'
 import Button from './button'
 import Icon from './icon'
+import Group from  './group'
 
 Vue.component('g-button', Button);
 Vue.component('g-icon', Icon);
+Vue.component('g-group', Group);
 
 new Vue({
   el: '#app',
