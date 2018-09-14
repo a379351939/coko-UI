12b84a54517c11f6d65885337e513df9e46319b6
tzc <491514218@qq.com>
2018-09-14 11:12:39 +0800

    加group

---
+++ src/group.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="g-group">
+    <slot></slot>
+  </div>
+</template>
+<script>
+  export default {
+    mounted(){
+      for (let node of this.$el.children) {
+        let name = node.nodeName.toLowerCase()
+        if(name !== 'button') {
+          console.warn(`group的子组件应全是g-button，但你写的却是${name}`)
+        }
+      }
+    }
+  }
+</script>
+<style lang="scss">
+  .g-group {
+    display: inline-flex;
+    vertical-align: middle;
+    > .g-button {
+      border-radius: 0;
+      &:not(:first-child) {
+        margin-left: -1px;
+      }
+      &:first-child {
+        border-top-left-radius: var(--border-radius);
+        border-bottom-left-radius: var(--border-radius);
+      }
+      &:last-child {
+        border-top-right-radius: var(--border-radius);
+        border-bottom-right-radius: var(--border-radius);
+      }
+      &:hover {
+        z-index: 1;
+      }
+    }
+  }
+</style>
+
