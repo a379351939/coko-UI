834c057e13d25e1b493edb647cc202a4e2a355ad
tzc <491514218@qq.com>
2018-09-19 22:51:46 +0800

    存档

--- src/app.js
+++ src/app.js
@@ -14,3 +14,50 @@ new Vue({
     loading2: false
   }
 });
+
+import chai from 'chai'
+
+const expect = chai.expect
+
+// 单元测试
+// {
+//   const Constructor = Vue.extend(Button)
+//   const button = new Constructor({
+//     propsData: {
+//       icon: 'settings'
+//     }
+//   })
+//
+//   button.$mount('#test')
+//   let useElement = button.$el.querySelector('use')
+//   expect(useElement.getAttribute('xlink:href')).to.eq('#i-settings')
+// }
+{
+  const Constructor = Vue.extend(Button)
+  const button = new Constructor({
+    propsData: {
+      icon: 'settings',
+      loading: true
+    }
+  })
+  button.$mount()
+  let useElement = button.$el.querySelector('use')
+  let href = useElement.getAttribute('xlink:href')
+  expect(href).to.eq('#i-loading')
+}
+{
+  {
+    const div = document.createElement('div')
+    document.body.appendChild(div)
+    const Constructor = Vue.extend(Button)
+    const button = new Constructor({
+      propsData: {
+        icon: 'settings'
+      }
+    })
+    button.$mount()
+    let svg = button.$el.querySelector('svg')
+    let {order} = window.getComputedStyle(svg)
+    expect(order).to.eq(1)
+  }
+}
