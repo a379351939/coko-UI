30a9176ce9bff0e2bbd3c972cfb85466d6fafed9
tzc <491514218@qq.com>
2018-09-21 00:06:43 +0800

    存档

--- src/app.js
+++ src/app.js
@@ -2,10 +2,12 @@ import Vue from 'vue'
 import Button from './button'
 import Icon from './icon'
 import Group from  './group'
+import Input from './input'
 
-Vue.component('g-button', Button);
-Vue.component('g-icon', Icon);
-Vue.component('g-group', Group);
+Vue.component('g-button', Button)
+Vue.component('g-icon', Icon)
+Vue.component('g-group', Group)
+Vue.component('g-input', Input)
 
 new Vue({
   el: '#app',
