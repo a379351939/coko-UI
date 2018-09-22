f81f9e1f28df208876f3e66caf01e69ad9836b87
tzc <491514218@qq.com>
2018-09-22 10:15:06 +0800

    存档

--- src/app.js
+++ src/app.js
@@ -3,11 +3,15 @@ import Button from './button'
 import Icon from './icon'
 import Group from  './group'
 import Input from './input'
+import Row from './row'
+import Col from  './col'
 
 Vue.component('g-button', Button)
 Vue.component('g-icon', Icon)
 Vue.component('g-group', Group)
 Vue.component('g-input', Input)
+Vue.component('g-row', Row)
+Vue.component('g-col', Col)
 
 new Vue({
   el: '#app',
