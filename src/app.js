371290067f86ca26c70907e45ae620d564730036
tzc <491514218@qq.com>
2018-09-26 14:59:17 +0800

    基本完成layout

--- src/app.js
+++ src/app.js
@@ -5,6 +5,11 @@ import Group from  './group'
 import Input from './input'
 import Row from './row'
 import Col from  './col'
+import Layout from './layout/layout'
+import Header from './layout/header'
+import Footer from './layout/footer'
+import Content from  './layout/content'
+import Side from './layout/side'
 
 Vue.component('g-button', Button)
 Vue.component('g-icon', Icon)
@@ -12,6 +17,11 @@ Vue.component('g-group', Group)
 Vue.component('g-input', Input)
 Vue.component('g-row', Row)
 Vue.component('g-col', Col)
+Vue.component('g-layout', Layout)
+Vue.component('g-header', Header)
+Vue.component('g-footer', Footer)
+Vue.component('g-content', Content)
+Vue.component('g-side', Side)
 
 new Vue({
   el: '#app',
